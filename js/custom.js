d2f0f908debacce612490f005f6970fef838ac9e
Maxim <maxim.maltsev@recogizer.de>
2017-11-03 14:40:08 +0100

    updated design

--- js/custom.js
+++ js/custom.js
@@ -1,40 +1,61 @@
 function setGuests() {
   guestArr = [{
+      names: ['Гости'],
+      sex: 'p',
+    }, {
       names: ['Мама', 'Папа'],
-      sex: '',
+      sex: 'p',
     }, {
       names: ['Максим'],
       sex: 'm',
     }, {
       names: ['Леночка', 'Саша', 'Ваня'],
-      sex: '',
+      sex: 'p',
     }, {
       names: ['Маша'],
       sex: 'f',
     }
   ]
   guestId = +window.location.search.substr(4)
-  let guests = guestArr[guestId]
+  let guests = guestArr[guestId] || guestArr[0]
   let dearEl = document.getElementById('dear')
-  let guestEl = document.getElementById('guests')
-  if (guests.names.length > 1) {
-    dearEl.innerText = 'Дорогие'
-    guestEl.innerHTML = ''
+  let guestEl1 = document.getElementById('guest1')
+  let guestEl2 = document.getElementById('guest2')
+  let guestEl3 = document.getElementById('guest3')
+  let delim1 = document.getElementById('delim1')
+  let delim2 = document.getElementById('delim2')
+  if (guests.names.length > 1 || guests.sex === 'p') {
+    dearEl.textContent  = 'Дорогие'
     for (let j=0; j < guests.names.length; j++) {
-      guestEl.innerHTML += '<u>' + guests.names[j] + '</u>'
-      if (j < guests.names.length - 2) {
-        guestEl.innerHTML += ', '
-      } else if (j == guests.names.length - 2) {
-        guestEl.innerHTML += ' и '
+      if (j == 0 && guests.names.length === 1) {
+        guestEl1.textContent  = ''
+        guestEl2.textContent  = ''
+        guestEl3.textContent  = guests.names[j]
+      } else if (j == 0 && guests.names.length === 2) {
+        guestEl1.textContent  = ''
+        guestEl2.textContent  = guests.names[j]
+        delim2.textContent  = 'и'
+      } else if (j == 0 && guests.names.length === 3) {
+        guestEl1.textContent  = guests.names[j]
+        delim1.textContent  = ','
+      } else if (j === 1 && guests.names.length === 2) {
+        guestEl3.textContent  = guests.names[j]
+      } else if (j === 1 && guests.names.length === 3) {
+        guestEl2.textContent  = guests.names[j]
+        delim2.textContent  = 'и'
+      } else if (j === 2) {
+        guestEl3.textContent  = guests.names[j]
       }
     }
   } else {
     if (guests.sex == 'm') {
-      dearEl.innerText = 'Дорогой'
+      dearEl.textContent  = 'Дорогой'
     } else {
-      dearEl.innerText = 'Дорогая'
+      dearEl.textContent  = 'Дорогая'
     }
-    guestEl.innerHTML =  '<u>' + guests.names[0] + '</u>'
+    guestEl1.textContent  = ''
+    guestEl2.textContent  = ''
+    guestEl3.textContent  = guests.names[0]
   }
 }
 
@@ -48,8 +69,8 @@ function setCounter() {
   setInterval(function() {
     let colons = document.getElementsByClassName('colon')
     for (let i = 0; i < colons.length; i ++) {
-      if (colons[i].innerText === ':') colons[i].innerText = ''
-      else colons[i].innerText = ':'
+      if (colons[i].textContent  === ':') colons[i].textContent  = ''
+      else colons[i].textContent  = ':'
     }
   }, 1000)
 }
@@ -64,22 +85,22 @@ function dayCalc(wdDate) {
 }
 
 function HTMLUpdate(dateDiff, hourDiff, minDiff) {
-  let dayDesc = document.getElementById('day-desc').innerText
+  let dayDesc = document.getElementById('day-desc').textContent 
   if (dateDiff % 10 > 4 || dateDiff % 10 === 0) dayDesc = 'Дней'
   else if (dateDiff % 10 > 1)                   dayDesc = 'Дня'
   else                                          dayDesc = 'День'
 
-  let hourDesc = document.getElementById('hour-desc').innerText
+  let hourDesc = document.getElementById('hour-desc').textContent 
   if (hourDiff % 10 > 4 || hourDiff % 10 === 0) hourDesc = 'Часов'
   else if (hourDiff % 10 > 1)                   hourDesc = 'Часа'
   else                                          hourDesc = 'Час'
 
-  let minDesc = document.getElementById('min-desc').innerText
+  let minDesc = document.getElementById('min-desc').textContent 
   if (minDiff % 10 > 4 || minDiff % 10 === 0) minDesc = 'Минут'
   else if (minDiff % 10 > 1)                  minDesc = 'Минуты'
   else                                        minDesc = 'Минута'
 
-  document.getElementById('day').innerText = '' + dateDiff
-  document.getElementById('hour').innerText = '' + hourDiff
-  document.getElementById('min').innerText = '' + minDiff
+  document.getElementById('day').textContent  = '' + dateDiff
+  document.getElementById('hour').textContent  = '' + hourDiff
+  document.getElementById('min').textContent  = '' + minDiff
 }
